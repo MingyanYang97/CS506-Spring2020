2f6fbbd012303cd013508162c6a1dfb50a090e65
MingyanYang97 <yangmingyan9724@126.com>
2020-02-01 13:48:32 -0500

    height

--- 01-python/infrastructure/tree.py
+++ 01-python/infrastructure/tree.py
@@ -1,7 +1,13 @@
-def draw_tree():
+def draw_tree(height=5):
+    if(height==0):
+        print("Trees not found")
+        return
+
     print("   /\ ")
     print("  /  \ ")
     print(" /    \ ")
     print("/______\ ")
+    for i in range(height-1):
+        print("  |  |")
     print("  |__|")
     return
