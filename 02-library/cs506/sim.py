9022466d7efa298ba968df1a9d47823c3f682e22
MingyanYang97 <yangmingyan9724@126.com>
2020-02-12 17:42:24 -0500

    up to date

--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,5 +1,12 @@
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+    if lrn(x) != len(y):
+        raise ValueError("not good values")
+
+    res = 0
+    for i in range(len(x)):
+        res += abs(x[i] - y[i])
+    return res ** (1/2) 
+    #raise NotImplementedError()
 
 def manhattan_dist(x, y):
     raise NotImplementedError()
